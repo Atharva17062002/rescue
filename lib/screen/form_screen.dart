18e5759a26ef04205689d3b2c942464921c1eed7
Akshay <aksh.ed@outlook.com>
2023-02-05 00:37:09 +0530

    Akon

--- lib/screen/form_screen.dart
+++ lib/screen/form_screen.dart
@@ -4,7 +4,15 @@ import '../Dbmodel.dart';
 import '../dbhelper/mongodb.dart';
 
 class FormScreen extends StatefulWidget {
-  const FormScreen({Key? key, required this.personalid,required this.lname,required this.fname,required this.location,required this.description,required this.imageurl}) : super(key: key);
+  const FormScreen(
+      {Key? key,
+      required this.personalid,
+      required this.lname,
+      required this.fname,
+      required this.location,
+      required this.description,
+      required this.imageurl})
+      : super(key: key);
 
   final String personalid;
   final String fname;
@@ -15,18 +23,19 @@ class FormScreen extends StatefulWidget {
   @override
   State<FormScreen> createState() => _FormScreenState();
 }
+
 class _FormScreenState extends State<FormScreen> {
-  // // final _formkey = GlobalKey<FormState>();
+  final _formkey = GlobalKey<FormState>();
   // final TextEditingController _name = TextEditingController();
   // final TextEditingController _pid = TextEditingController();
   // final TextEditingController _location = TextEditingController();
   // final TextEditingController _des = TextEditingController();
-  // late String name ;
-  // late String pid ;
-  // late String location ;
-  // late String des ;
+  // late String name;
+  // late String pid;
+  // late String location;
+  // late String des;
   // late String imageurl;
-  //
+
   // void initState() {
   //   _name.text = name;
   //   _pid.text = pid;
@@ -39,42 +48,42 @@ class _FormScreenState extends State<FormScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child:Column(
-            children: [
-              SizedBox(height: MediaQuery.of(context).size.height*0.05,),
-              Container(
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: Image.network(
-                    widget.imageurl,
-                    width: 50,
-                    height: 100,
-                    fit: BoxFit.fill,
-                  )),
-              SizedBox(
-                height: MediaQuery.of(context).size.height*0.05,
+          child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height * 0.35,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Image.network(
+                widget.imageurl,
+                width: 50,
+                height: 100,
+                fit: BoxFit.fill,
+              )),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5), color: Colors.grey),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  buildTextField('Personal ID : ', widget.personalid),
+                  buildTextField('Full Name : ', widget.fname),
+                  buildTextField('Location : ', widget.location),
+                  buildTextField('Description : ', widget.description),
+                ],
               ),
-
-              Container(
-                height: MediaQuery.of(context).size.height * 0.45,
-                width: MediaQuery.of(context).size.width * 0.9,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5), color: Colors.grey),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      buildTextField('Personal ID : ', widget.personalid),
-                      buildTextField('Full Name : ', widget.fname),
-                      buildTextField('Location : ', widget.location),
-                      buildTextField('Description : ', widget.description ),
-                    ],
-                  ),
-                ),
-              )
-            ],
+            ),
           )
-      ),
+        ],
+      )),
       // bottomNavigationBar: Row(
       //   children: [
       //     SizedBox(
@@ -108,9 +117,12 @@ class _FormScreenState extends State<FormScreen> {
   // }
   Widget buildTextField(String labelText, String info) {
     return Padding(
-      padding: EdgeInsets.only (bottom: 30),
+      padding: EdgeInsets.only(bottom: 30),
       child: TextField(
-        decoration: InputDecoration (
+        onSubmitted: (text) {
+          MongoDatabase.setData(widget.personalid, text);
+        },
+        decoration: InputDecoration(
           contentPadding: EdgeInsets.only(bottom: 5),
           labelText: labelText,
           floatingLabelBehavior: FloatingLabelBehavior.always,
@@ -121,5 +133,3 @@ class _FormScreenState extends State<FormScreen> {
     );
   }
 }
-
-
