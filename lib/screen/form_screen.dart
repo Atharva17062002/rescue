e6fec27c59370712e87550b4449cf2dcb4ad9499
Atharva Udavant <17.atharva@gmail.com>
2023-02-05 00:01:09 +0530

    H

--- lib/screen/form_screen.dart
+++ lib/screen/form_screen.dart
@@ -4,126 +4,108 @@ import '../Dbmodel.dart';
 import '../dbhelper/mongodb.dart';
 
 class FormScreen extends StatefulWidget {
-  const FormScreen({Key? key, }) : super(key: key);
-  //
-  // String personalid;
-  // String fname;
-  // String lname;
-  // String imageurl;
-  // String location;
-  // String description;
+  const FormScreen({Key? key, required this.personalid,required this.lname,required this.fname,required this.location,required this.description,required this.imageurl}) : super(key: key);
+
+  final String personalid;
+  final String fname;
+  final String lname;
+  final String imageurl;
+  final String location;
+  final String description;
   @override
   State<FormScreen> createState() => _FormScreenState();
 }
 class _FormScreenState extends State<FormScreen> {
-  // final _formkey = GlobalKey<FormState>();
-  final TextEditingController _name = TextEditingController();
-  final TextEditingController _pid = TextEditingController();
-  final TextEditingController _location = TextEditingController();
-  final TextEditingController _des = TextEditingController();
-  late String name ;
-  late String pid ;
-  late String location ;
-  late String des ;
-  late String imageurl;
-
-  void initState() {
-    _name.text = name;
-    _pid.text = pid;
-    _location.text = location;
-    _des.text = des;
-    super.initState();
-  }
+  // // final _formkey = GlobalKey<FormState>();
+  // final TextEditingController _name = TextEditingController();
+  // final TextEditingController _pid = TextEditingController();
+  // final TextEditingController _location = TextEditingController();
+  // final TextEditingController _des = TextEditingController();
+  // late String name ;
+  // late String pid ;
+  // late String location ;
+  // late String des ;
+  // late String imageurl;
+  //
+  // void initState() {
+  //   _name.text = name;
+  //   _pid.text = pid;
+  //   _location.text = location;
+  //   _des.text = des;
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: FutureBuilder(
-              future: MongoDatabase.getData(),
-              builder: (context,AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  if (snapshot.hasData) {
-                     return ListView.builder(
-                        itemCount: 1,
-                        itemBuilder: (BuildContext context, index) {
-                          return
-                          display(MongoDbModel.fromJson(snapshot.data[index]));
-                        });
-                  } else {
-                    return Center(
-                      child: Text("No data"),
-                    );
-                  }
-                }
-              }
-          )
-      ),
-      bottomNavigationBar: Row(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.03,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Reject'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.red),
-              //maximumSize: MaterialStateProperty.all(Size(2, 2))
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.5,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Accept'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.green),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget display(MongoDbModel data){
-    return Column(
-      children: [
-        Container(
-            height: MediaQuery.of(context).size.height * 0.35,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: Image.network(
-              '${data.imageurl}',
-              width: 50,
-              height: 100,
-              fit: BoxFit.fill,
-            )),
-        SizedBox(
-          height: MediaQuery.of(context).size.height*0.05,
-        ),
-
-        Container(
-          height: MediaQuery.of(context).size.height * 0.45,
-          width: MediaQuery.of(context).size.width * 0.9,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5), color: Colors.grey),
-          child: Column(
+      body: Center(
+          child:Column(
             children: [
-              buildTextField('Personal ID : ', '${data.personalid}'),
-              buildTextField('Full Name : ', '${data.fname + data.lname}'),
-              buildTextField('Location : ', '${data.location}'),
-              buildTextField('Description : ', '${data.description}'),
+              SizedBox(height: MediaQuery.of(context).size.height*0.05,),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Image.network(
+                    widget.imageurl,
+                    width: 50,
+                    height: 100,
+                    fit: BoxFit.fill,
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.05,
+              ),
+
+              Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5), color: Colors.grey),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      buildTextField('Personal ID : ', widget.personalid),
+                      buildTextField('Full Name : ', widget.fname),
+                      buildTextField('Location : ', widget.location),
+                      buildTextField('Description : ', widget.description ),
+                    ],
+                  ),
+                ),
+              )
             ],
-          ),
-        )
-      ],
+          )
+      ),
+      // bottomNavigationBar: Row(
+      //   children: [
+      //     SizedBox(
+      //       width: MediaQuery.of(context).size.width * 0.03,
+      //     ),
+      //     ElevatedButton(
+      //       onPressed: () {},
+      //       child: Text('Reject'),
+      //       style: ButtonStyle(
+      //         backgroundColor: MaterialStateProperty.all(Colors.red),
+      //         //maximumSize: MaterialStateProperty.all(Size(2, 2))
+      //       ),
+      //     ),
+      //     SizedBox(
+      //       width: MediaQuery.of(context).size.width * 0.5,
+      //     ),
+      //     ElevatedButton(
+      //       onPressed: () {},
+      //       child: Text('Accept'),
+      //       style: ButtonStyle(
+      //         backgroundColor: MaterialStateProperty.all(Colors.green),
+      //       ),
+      //     )
+      //   ],
+      // ),
     );
   }
+
+  // Widget display(MongoDbModel data){
+  //   return
+  // }
   Widget buildTextField(String labelText, String info) {
     return Padding(
       padding: EdgeInsets.only (bottom: 30),
@@ -140,4 +122,4 @@ class _FormScreenState extends State<FormScreen> {
   }
 }
 
-// required this.personalid,required this.lname,required this.fname,required this,required this.personalid,required this.personalid
+
