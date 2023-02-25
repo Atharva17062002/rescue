a196efa0c60662f6b62486fd08756ce16d1f1c5a
Akshay <aksh.ed@outlook.com>
2023-02-25 12:47:59 +0530

    Update options added with flag check

--- lib/Dbmodel.dart
+++ lib/Dbmodel.dart
@@ -6,21 +6,22 @@ import 'dart:convert';
 
 import 'package:mongo_dart/mongo_dart.dart';
 
-MongoDbModel mongoDbModelFromJson(String str) => MongoDbModel.fromJson(json.decode(str));
+MongoDbModel mongoDbModelFromJson(String str) =>
+    MongoDbModel.fromJson(json.decode(str));
 
 String mongoDbModelToJson(MongoDbModel data) => json.encode(data.toJson());
 
 class MongoDbModel {
-  MongoDbModel({
-    required this.id,
-    required this.personalid,
-    required this.fname,
-    required this.lname,
-    required this.imageurl,
-    required this.location,
-    required this.description,
-    required this.v,
-  });
+  MongoDbModel(
+      {required this.id,
+      required this.personalid,
+      required this.fname,
+      required this.lname,
+      required this.imageurl,
+      required this.location,
+      required this.description,
+      required this.v,
+      required this.assignedStatus});
 
   ObjectId id;
   String personalid;
@@ -30,30 +31,31 @@ class MongoDbModel {
   String location;
   String description;
   int v;
+  bool assignedStatus;
 
   factory MongoDbModel.fromJson(Map<String, dynamic> json) => MongoDbModel(
-
-  id: json["_id"],
-  personalid: json["personalid"],
-  fname: json["fname"],
-  lname: json["lname"],
-  imageurl: json["imageurl"],
-  location: json["location"],
-  description: json["description"],
-  v: json["__v"],
-  );
+        id: json["_id"],
+        personalid: json["personalid"],
+        fname: json["fname"],
+        lname: json["lname"],
+        imageurl: json["imageurl"],
+        location: json["location"],
+        description: json["description"],
+        v: json["__v"],
+        assignedStatus: json["assignedStatus"],
+      );
 
   Map<String, dynamic> toJson() => {
-
-    "_id": id.toJson(),
-    "personalid": personalid,
-    "fname": fname,
-    "lname": lname,
-    "imageurl": imageurl,
-    "location": location,
-    "description": description,
-    "__v": v,
-  };
+        "_id": id.toJson(),
+        "personalid": personalid,
+        "fname": fname,
+        "lname": lname,
+        "imageurl": imageurl,
+        "location": location,
+        "description": description,
+        "__v": v,
+        "assigedStatus": assignedStatus
+      };
 }
 
 class Id {
@@ -64,10 +66,10 @@ class Id {
   String oid;
 
   factory Id.fromJson(Map<String, dynamic> json) => Id(
-    oid: json["\u0024oid"],
-  );
+        oid: json["\u0024oid"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "\u0024oid": oid,
-  };
+        "\u0024oid": oid,
+      };
 }
